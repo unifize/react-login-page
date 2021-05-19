c4b5b1eeb6e287a8b755c9fee5fb113e9cafb47b
Tulsi Prasad <tulsi.prasad50@gmail.com>
2021-05-19 18:04:52 +0530

    Setup basic project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Jumbotron, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Acme Limited</h1>
+      </Jumbotron>
+      <Button size="lg" variant="dark" style={{ marginRight: "1rem" }}>
+        <Link to="/login" style={{ color: "white", textDecoration: "none" }}>
+          Login
+        </Link>
+      </Button>
+      <Button size="lg" variant="dark">
+        <Link to="/signup" style={{ color: "white", textDecoration: "none" }}>
+          Signin
+        </Link>
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
